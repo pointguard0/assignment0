4963ef7f000f2672701a13bda0473df91eaebfe6
Nvard92 <nvardsiradeghyan1992@gmail.com>
2017-09-13 11:12:15 +0400

    initial commit

---
+++ Nvard92/assignment0.sh
@@ -0,0 +1,32 @@
+#set -x
+
+function print(){
+echo $1
+}
+
+
+function checkArgs(){
+
+if [ ! -n "$1" ]
+then 
+      print "The file to be checked is missing"
+      exit
+else 
+  if [ ! -n "$2" ]
+  then 
+    print "The dictionary is not given"
+    exit
+  fi
+fi
+}
+
+function spell()
+{
+
+fmt -1 $1|sort|tr A-Z a-z| comm -13  $2  -
+
+}
+
+checkArgs
+spell $1
+
